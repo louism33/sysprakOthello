600b459532d5458fa4d9ab585d0165f7ef7a2ebc
bianca9927 <1962163657@qq.com>
2019-11-13 19:13:18 +0100

    neue define

--- connector/connector.c
+++ connector/connector.c
@@ -1,11 +1,15 @@
 //
 // Created by louis on 11/13/19.
 //
-
 #include "connector.h"
 #include <stdio.h>
+#define GAMEKINDNAME "Reversi"
+#define PORTNUMBER 1357
+#define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 
 void connectorMasterMethod(){
     printf("Hi I am good at connecting\n");
 
 }
+
+

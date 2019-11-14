893e1cac64f5d75c370e26a59d67c06d9099014e
bianca9927 <1962163657@qq.com>
2019-11-14 10:46:05 +0100

    main-Funktion mit Kommando

--- main.c
+++ main.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
 #include "connector/connector.h"
 #include "thinker/thinker.h"
 
-int main() {
+int main(int argc,char*argv[]) {
+    char *GAMEID;
+    int GAMENUMBER=0;
+    int ret;
+    while((ret=getopt(argc, argv, "g:p:")) != -1) {
+        switch(ret){
+            case 'g':
+                GAMEID=optarg;
+                break;
+            case 'p':
+                GAMENUMBER=atoi(optarg);
+                break;
+            default:
+                perror("keine gültige Game-ID oder Game-Number.\n");
+                break;
+        }
+    }
+    //Fehlerbehandelung
+   if(strlen(GAMEID)>13){
+        perror("Das Game-ID ist grosser als 13-stellige.\n");
+        GAMEID=NULL;
+        exit(0);
+    }else if(strlen(GAMEID)<13){
+        perror("Das Game-ID ist kleiner als 13-stellige.\n");
+        GAMEID=NULL;
+        exit(0);
+    } 
     printf("Hello World! I am Alex. This is the main method\n");
-
     connectorMasterMethod();
     thinkerMasterMethod();
+    
 
 
     return 0;

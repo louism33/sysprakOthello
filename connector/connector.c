1cb2e8f34dac6a65f10351a2beb9abb2ab03a01f
bianca9927 <1962163657@qq.com>
2019-11-14 21:56:32 +0100

    neue

--- connector/connector.c
+++ connector/connector.c
@@ -3,13 +3,46 @@
 //
 #include "connector.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
 #define GAMEKINDNAME "Reversi"
 #define PORTNUMBER 1357
 #define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 
-void connectorMasterMethod(){
-    printf("Hi I am good at connecting\n");
-
+void connectorMasterMethod(int argc,char*argv[]){
+ printf("Hi I am good at connecting\n");
+     char *GAMEID;
+    int GAMENUMBER=0;
+    int ret;
+    while((ret=getopt(argc, argv, "g:p:")) != -1) {
+        switch(ret){
+            case 'g':
+                GAMEID=optarg;
+                break;
+            case 'p':
+                GAMENUMBER=atoi(optarg);
+                break;
+            default:
+                perror("keine gültige Game-ID oder Game-Number.\n");
+                break;
+        }
+    }
+    //Fehlerbehandelung
+   if(strlen(GAMEID)>13){
+        perror("Das Game-ID ist grosser als 13-stellige.\n");
+        GAMEID=NULL;
+        exit(0);
+    }else if(strlen(GAMEID)<13){
+        perror("Das Game-ID ist kleiner als 13-stellige.\n");
+        GAMEID=NULL;
+        exit(0);
+    }
 }
+
+
+
+
 
 

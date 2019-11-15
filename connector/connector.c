23c63276a274340ade07f32eb7d4e9b39509c8a9
Louis <louis.mackenziesmith@googlemail.com>
2019-11-15 19:51:33 +0100

    resolve hostname to IP address

--- connector/connector.c
+++ connector/connector.c
@@ -25,11 +25,74 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+
 #define GAMEKINDNAME "Reversi"
 #define PORTNUMBER 1357
 #define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 
+char * lookup_host (const char *host) {
+    struct addrinfo hints, *res;
+    int errcode;
+    char addrstr[100];
+    char* finalAddrstr;
+    void *ptr;
 
+    finalAddrstr = malloc(sizeof(char) * 200);
+
+    memset (&hints, 0, sizeof (hints));
+    hints.ai_family = PF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags |= AI_CANONNAME;
+
+    errcode = getaddrinfo (host, NULL, &hints, &res);
+    if (errcode != 0)
+    {
+        perror ("getaddrinfo");
+        return "x";
+    }
+
+    printf ("Host: %s\n", host);
+    while (res)
+    {
+        inet_ntop (res->ai_family, res->ai_addr->sa_data, addrstr, 100);
+
+//        printf("addrstr:    %s \n", addrstr);
+
+        switch (res->ai_family)
+        {
+            case AF_INET:
+                ptr = &((struct sockaddr_in *) res->ai_addr)->sin_addr;
+                break;
+            case AF_INET6:
+                ptr = &((struct sockaddr_in6 *) res->ai_addr)->sin6_addr;
+                break;
+        }
+        inet_ntop (res->ai_family, ptr, addrstr, 100);
+        printf ("IPv%d address: %s (%s)\n", res->ai_family == PF_INET6 ? 6 : 4,
+                addrstr, res->ai_canonname);
+
+        if (res->ai_family != PF_INET6) {
+            strcpy(finalAddrstr, addrstr);
+            return finalAddrstr;
+        }
+        res = res->ai_next;
+    }
+
+    return finalAddrstr;
+
+//    return "";
+}
 
 
 int connectToGameServer(int mockGame) {
@@ -57,11 +120,25 @@ int connectToGameServer(int mockGame) {
     server.sin_family = PF_INET;
 
     if (mockGame) {
-        server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    } else {
-        server.sin_addr.s_addr = inet_addr(HOSTNAME);
+        char *local = "127.0.0.1";
+        server.sin_addr.s_addr = inet_addr(local);
+        printf("Attempting to connect to host %s on port %d\n", local, PORTNUMBER);
 
+    } else {
+
+        char *localh = "www.cnn.com";
+
+
+        char *host = lookup_host(HOSTNAME);
+
+        printf("Attempting to connect to host %s on port %d\n", host, PORTNUMBER);
+
+        server.sin_addr.s_addr = inet_addr(HOSTNAME);
     }
+
+
+
+
     server.sin_port = htons(PORTNUMBER);
 
     // connect the client socket to the server socket

b2d5dbf526863919913210d6d092bdff46305874
bianca9927 <1962163657@qq.com>
2019-11-17 13:23:07 +0100

    main

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc,char*argv[]) {
     printf("Hello World! I am Alex. This is the main method\n");
     thinkerMasterMethod();
     connectorMasterMethod(argc,argv);
-    
+
 
 
     return 0;

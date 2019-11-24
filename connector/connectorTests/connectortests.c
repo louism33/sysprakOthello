a465375a022adbaadf611b6e8e741f1463ddd9ab
bianca9927 <1962163657@qq.com>
2019-11-24 19:33:55 +0100

    neu test

--- connector/connectorTests/connectortests.c
+++ connector/connectorTests/connectortests.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "../../thinker/board.h"
 #include "../performConnection.h"
 
-
-int testConvertMove(){
-    if(convertMove(0) !="A8"){
-        printf("there is a problem in convertMove");
+int testConvertMove()
+{
+    char test[] = convertMove(0);
+    if (strcmp(test, "A8") != 0)
+    {
+        printf("there is a problem in convertMove\n");
         exit(1);
     }
     return 0;

7fa1ff1190573a818fcf6320fc57642c74c70399
louis <louis.mackenziesmith@googlemail.com>
2020-01-06 19:32:14 +0100

    e

--- main.c
+++ main.c
@@ -11,6 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <signal.h>
 #include <string.h>
 #include "connector/connector.h"

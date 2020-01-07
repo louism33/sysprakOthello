ad2c3c28bbf9807a4b91de3fa8f75a92ceb968c9
louis <louis.mackenziesmith@googlemail.com>
2020-01-07 17:26:27 +0100

    e

--- scripts/sysprak-abgabe.sh
+++ scripts/sysprak-abgabe.sh
@@ -101,6 +101,7 @@ echo "STARTING PLAYER1"
 
 ## start PLAYER1
 #GAME_ID=$ID PLAYER=$PLAYER1 make play &>> p1.txt &
+#GAME_ID=$ID PLAYER=$PLAYER1 CONFIG_FILE=test-client.conf make play &
 GAME_ID=$ID PLAYER=$PLAYER1 make play &
 
 # trying to get perf info

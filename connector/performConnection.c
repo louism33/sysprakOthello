8d646e0deaf0f2efad5c24aeaa72f1bdee0a8f24
louis <louis.mackenziesmith@googlemail.com>
2020-01-13 23:47:04 +0100

    e

--- connector/performConnection.c
+++ connector/performConnection.c
@@ -229,13 +229,14 @@ int readNextLine(int socket, char *buffer, int sizeOfBuff) {
         i++;
 
         if (readResponse = read(socket, myInternalBuffer + bytesRead, sizeOfBuff-bytesRead)) {
+            bytesRead += readResponse;
             if ((lineBreak = hasLineBreak(myInternalBuffer, readResponse+bytesRead, bytesRead)) == -1) {
                 printf("            no line break found!! internal buff:  '%s' \n", myInternalBuffer);
                 continue;
             }
             printf("           LINE BREAK FOUND, index: %d!! internal buff:  '%s' \n", lineBreak, myInternalBuffer);
 
-            bytesRead += readResponse;
+
 
             strncpy(buffer, myInternalBuffer, sizeOfBuff); // change to bytesRead maybe
             bzero(myInternalBuffer, sizeOfBuff);
